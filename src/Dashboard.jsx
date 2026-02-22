6d3c6a2797135eb08085e9b89fee6cbdc994842e
jprepa <164268561+jprepa@users.noreply.github.com>
2026-02-22 17:39:31 -0300

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -97,7 +97,7 @@ const onComplete = async () => {
     // Mensagem personalizada dependendo do tipo
     if (isFake) {
       // Mensagem mais séria, sem "Parabéns"
-      alert(` ${raspadinha.premios?.nome}.\nContinue indicando empresas para ganhar!\n Se tiver alguma dúvida entre em contato com alguém do rime de R&E.`);
+      alert(`${raspadinha.premios?.nome}.\nNão desanime, continue indicando empresas para ganhar sua raspadinha!\nSe tiver alguma dúvida entre em contato com o time de R&E.`);
     } else {
       // Mensagem de festa normal
       alert(`PARABÉNS! Você ganhou: ${raspadinha.premios?.nome || 'Um Prêmio!'}`);
